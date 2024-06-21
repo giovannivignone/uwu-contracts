e74c1098aa822d6f509e9a8e9dee4407a78e1e10
Giovanni vignone <giovannivignone@Giovannis-MBP.localdomain>
2024-06-21 15:52:22 -0700

    new correct code

--- src/staking/interfaces/IStakingRewards.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-interface IStakingRewards {
-  function lock(address who, uint _amount) external;
-  function withdraw(address who, uint _amount) external;
-}
